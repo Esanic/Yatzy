1e4621c71b06b91747d83303091eef37ee98263e
Christopher Reineborn <christopher@zylion.se>
2022-11-11 10:02:31 +0100

    added socket.io frontend and backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { environment } from 'src/environments/environment';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+const config: SocketIoConfig = {
+  url: environment.socketUrl,
+  options: {
+    transports: ['websocket']
+  }
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
