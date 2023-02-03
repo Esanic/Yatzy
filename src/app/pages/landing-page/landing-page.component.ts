9baa7739e5ad90533b9932717549cb15c9bfd998
Christopher Reineborn <christopher.reineborn@jsc.se>
2023-02-03 13:41:14 +0100

    Part 2 of potential bugfix regarding redirections.

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router) {
+    _router.events.forEach((event) => {
+      if(event instanceof NavigationStart) {
+        if (event.navigationTrigger === 'popstate') {
+          _router.navigate([''], {skipLocationChange: true})
+        }
+      }
+    });
+   }
 
   ngOnInit(): void {
   }
